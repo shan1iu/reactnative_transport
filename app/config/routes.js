c37100609f443ee473c4b6bb7cae7193c75178a8
shan1iu <mountainliuliu@gmail.com>
2018-06-09 13:35:36 +0100

    upgrading react-navigation v1 -> v2

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,57 +1,70 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-import {StackNavigator, TabBarBottom, TabNavigator} from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import {
+  TabBarBottom,
+  TabNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import Home from '../screens/Home';
-import Fav from '../screens/Fav';
-import Route from '../screens/Routes';
+import Home from "../screens/Home";
+import Fav from "../screens/Fav";
+import Route from "../screens/Routes";
 import Settings from "../screens/Settings";
 import Search from "../screens/Search";
 
-
-export default TabNavigator(
-	{
-		Home: {screen: Home},
-		Fav: {screen: Fav},
-		Route: {screen: Route},
-		Search: {screen: Search},
-		Settings: {screen: Settings},
-	},
-	{
-		navigationOptions: ({navigation}) => ({
-			tabBarIcon: ({focused, tintColor}) => {
-				const {routeName} = navigation.state;
-				let iconName;
-				if (routeName === 'Home') {
-					iconName = "md-home";
-				} else if (routeName === 'Fav') {
-					iconName = "md-heart";
-				} else if (routeName === 'Route') {
-					iconName = "md-swap";
-				} else if (routeName === 'Settings') {
-					iconName = "md-settings";
-				} else if (routeName === 'Search') {
-					iconName = "md-search";
-				}
-				// You can return any component that you like here! We usually use an
-				// icon component from react-native-vector-icons
-				return <Ionicons name={iconName} size={30} color={tintColor}/>;
-			},
-		}),
-		tabBarOptions: {
-			activeTintColor: '#F64A4A',
-			inactiveTintColor: '#b0b0b0',
-			showLabel: false,
-			style: {
-				borderTopWidth: 1,
-				backgroundColor: "transparent",
-			}
-		},
-		tabBarComponent: TabBarBottom,
-		tabBarPosition: 'bottom',
-		animationEnabled: false,
-		swipeEnabled: false,
-	}
+export default createBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        labeled: false,
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-home" size={30} color={tintColor} />
+        )
+      }
+    },
+    Fav: {
+      screen: Fav,
+      navigationOptions: {
+        labeled: false,
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-heart" size={30} color={tintColor} />
+        )
+      }
+    },
+    Route: {
+      screen: Route,
+      navigationOptions: {
+        labeled: false,
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-swap" size={30} color={tintColor} />
+        )
+      }
+    },
+    Search: {
+      screen: Search,
+      navigationOptions: {
+        labeled: false,
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-search" size={30} color={tintColor} />
+        )
+      }
+    },
+    Settings: {
+      screen: Settings,
+      navigationOptions: {
+        labeled: false,
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="md-settings" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#F64A4A",
+      inactiveTintColor: "#b0b0b0",
+      showLabel: false
+    }
+  }
 );
-
