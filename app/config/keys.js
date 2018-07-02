459494cd645bd1bc3f39b9bcff21455476bce166
shan1iu <mountainliuliu@gmail.com>
2018-07-02 01:23:59 +0100

    adding app/config/keys.js

---
+++ app/config/keys.js
@@ -0,0 +1,7 @@
+export const USERKEY1 =
+  "app_id=2ab3f5e2&app_key=4f694a46d98dde70516abbc1f636b93a";
+export const USERKEY2 =
+  "app_id=21531a5c&app_key=aa1b4a07c862e7cfdb7dafb23bf888a9";
+export const TFGM_KEY = {
+  "Ocp-Apim-Subscription-Key": "352d46528de74094835792b951c110fc"
+};
